d5d1c8f5d5c32e0eb21c49ac10d2af4811136af0
Surya Arun <132573060+Surya-A-Pandian@users.noreply.github.com>
2023-05-18 17:20:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,138 @@
+$('document').ready(function(){
+    $('.jsc').hide()
+$(".jsa").click(function(){
+$(".p1").slideToggle()
+$(".p2").slideUp()
+$(".jsc1").hide()
+$(".jsa1").show()
+$(".jsc2").hide()
+$(".jsa2").show()
+$(".jsc3").hide()
+$(".jsa3").show()
+$(".p3").slideUp()
+$(".p4").slideUp()
+$('.jsa').hide()
+$('.jsc').show()
+});
+});
+
+$('document').ready(function(){
+$(".jsc").click(function(){
+$(".p1").slideToggle()
+$('.jsc').hide()
+$('.jsa').show()
+});
+});
+
+// second
+
+$('document').ready(function(){
+    $('.jsc1').hide()
+$(".jsa1").click(function(){
+$(".p2").slideToggle()
+$(".p1").slideUp()
+$(".jsc").hide()
+$(".jsa").show()
+$(".jsc2").hide()
+$(".jsa2").show()
+$(".jsc3").hide()
+$(".jsa3").show()
+$(".p3").slideUp()
+$(".p4").slideUp()
+$('.jsa1').hide()
+$('.jsc1').show()
+});
+});
+
+$('document').ready(function(){
+    // $('.jsa').hide()
+$(".jsc1").click(function(){
+$(".p2").slideToggle()
+$('.jsc1').hide()
+$('.jsa1').show()
+});
+});
+
+// Third
+
+
+$('document').ready(function(){
+    $('.jsc2').hide()
+$(".jsa2").click(function(){
+$(".p3").slideToggle()
+$(".p1").slideUp()
+$(".jsc").hide()
+$(".jsa").show()
+$(".jsc1").hide()
+$(".jsa1").show()
+$(".jsc3").hide()
+$(".jsa3").show()
+$(".p2").slideUp()
+$(".p4").slideUp()
+
+$('.jsa2').hide()
+$('.jsc2').show()
+});
+});
+
+$('document').ready(function(){
+    // $('.jsa').hide()
+$(".jsc2").click(function(){
+$(".p3").slideToggle()
+$('.jsc2').hide()
+$('.jsa2').show()
+});
+});
+
+
+//Four
+
+
+$('document').ready(function(){
+    $('.jsc3').hide()
+$(".jsa3").click(function(){
+$(".p4").slideToggle()
+$(".p1").slideUp()
+$(".jsc").hide()
+$(".jsa").show()
+$(".jsc2").hide()
+$(".jsa2").show()
+$(".jsc1").hide()
+$(".jsa1").show()
+$(".p2").slideUp()
+$(".p3").slideUp()
+
+$('.jsa3').hide()
+$('.jsc3').show()
+});
+});
+
+$('document').ready(function(){
+    // $('.jsa').hide()
+$(".jsc3").click(function(){
+$(".p4").slideToggle()
+$('.jsc3').hide()
+$('.jsa3').show()
+});
+});
+
+
+
+
+// $('document').ready(function(){
+// $(".card1").click(function(){
+// $("p2").slideToggle()
+// });
+// });
+
+// $('document').ready(function(){
+// $(".card2").click(function(){
+// $("p3").slideToggle()
+// });
+// });
+
+// $('document').ready(function(){
+// $(".card3").click(function(){
+// $("p4").slideToggle()
+// });
+// });
